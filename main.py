fe925c4ec293481f59a6452c8ff7ef588f02c203
Anthony <anthonychinaemeru84@yahoo.com>
2024-02-04 11:41:59 +0100

    Done. Added Enhancer and other methods.

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ out_path = './editedImg'
 for filename in os.listdir(in_paths):
     img = Image.open(f"{in_paths}/{filename}")
     edit = img.filter(ImageFilter.SHARPEN).convert("L")
+    factor = 1.5
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
+    clean_name = os.path.splitext(filename)[0]
+    edit.save(f"{out_path}/{clean_name}_edited.jpg")
